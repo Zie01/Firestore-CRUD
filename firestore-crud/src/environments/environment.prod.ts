dda5971ac55b0988cb7d496a1558905ed26a5312
jemmel <yragrande01@gmail.com>
2018-01-12 14:34:51 -0800

    angularsf and firestore_crud

---
+++ firestore-crud/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyCkyR5UZNHMoT4isrQzgyA1t8EE1C_vzIc",
+    authDomain: "firestore-crud-dcdf7.firebaseapp.com",
+    databaseURL: "https://firestore-crud-dcdf7.firebaseio.com",
+    projectId: "firestore-crud-dcdf7",
+    storageBucket: "firestore-crud-dcdf7.appspot.com",
+    messagingSenderId: "800674459983"
+
+};
---
+++ firestore-crud/src/environments/environment.ts
@@ -0,0 +1,15 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
+export const environment = {
+  production: false,
+  apiKey: "AIzaSyCkyR5UZNHMoT4isrQzgyA1t8EE1C_vzIc",
+    authDomain: "firestore-crud-dcdf7.firebaseapp.com",
+    databaseURL: "https://firestore-crud-dcdf7.firebaseio.com",
+    projectId: "firestore-crud-dcdf7",
+    storageBucket: "firestore-crud-dcdf7.appspot.com",
+    messagingSenderId: "800674459983"
+
+};
